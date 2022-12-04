86ec4a0dd42951a60b9102b110a763da96670d82
SumanthRH <sumanthrh99@gmail.com>
2022-12-04 15:56:56 -0800

    MOD: cleaned repo

--- utils/sbert_meme_classifier.py
+++ utils/sbert_meme_classifier.py
@@ -13,7 +13,7 @@ models_path = 'models/{}'
 embeddings_path = 'models/model_utils/{}/category_embeddings.pkl'
 
 class Classifier:
-    def __init__(self, labels_path='data/training_label.pkl', model_name=None, k=3):
+    def __init__(self, model_name=None, k=3):
         self.model_name = model_name if model_name else base_model_name
         self.model_path = models_path.format(self.model_name)
         self.embedding_path = embeddings_path.format(self.model_name)
@@ -26,14 +26,6 @@ class Classifier:
         # load model embeddings
         with open(self.embedding_path, 'rb') as f:
             self.embeddings = pickle.load(f)
-
-        # load training labels and create reverse dict
-        with open(labels_path, 'rb') as f:
-            self.uuid_labels_dict = pickle.load(f)
-            self.labels_uuid_dict = {v: k for k, v in self.uuid_labels_dict.items()}
-        
-        self.num_classes = len(self.uuid_labels_dict)
-            
     def predictTopK(self, text):
         embedding = self.model.encode(text)
         scores = [(cos_sim(embedding, v), key) for key, v in self.embeddings.items()]
